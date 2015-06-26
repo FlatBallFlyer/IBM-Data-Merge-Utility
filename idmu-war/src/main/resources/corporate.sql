1a78bfd7c07b48a4adebc257acc2a351c08125df
Mike Storey <mike@ssgpp.com>
2015-06-26 08:27:24 -0400

    Database Script Updates

---
+++ idmu-war/src/main/resources/corporate.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: SELECT * FROM testgen.corporate
+LIMIT 0, 1000
+
+-- Date: 2015-06-25 18:14
+*/
+INSERT INTO `corporate` (`IDCORPORATE`,`FROM_VALUE`,`TO_VALUE`) VALUES (1,'corpUrl','www.spacely.com');
+INSERT INTO `corporate` (`IDCORPORATE`,`FROM_VALUE`,`TO_VALUE`) VALUES (2,'corpStreet','101 Future Ave.');
+INSERT INTO `corporate` (`IDCORPORATE`,`FROM_VALUE`,`TO_VALUE`) VALUES (3,'corpCity','Space City');
+INSERT INTO `corporate` (`IDCORPORATE`,`FROM_VALUE`,`TO_VALUE`) VALUES (4,'corpState','IS');
+INSERT INTO `corporate` (`IDCORPORATE`,`FROM_VALUE`,`TO_VALUE`) VALUES (5,'corpZip','99353');
---
+++ idmu-war/src/main/resources/customer.sql
@@ -0,0 +1,26 @@
+/*
+-- Query: SELECT * FROM testgen.customer
+LIMIT 0, 1000
+
+-- Date: 2015-06-25 18:14
+*/
+INSERT INTO `customer` (`IDCUSTOMER`,`PRIMARY`,`NAME`,`REVENUE`,`PROFIT`,`STREET`,`CITY`,`STATE`,`ZIP`,`PHONE`) VALUES (1,'James','General Motors',9824,806,'5791 Pleasant Prairie End','Dysart','PA','16188-0761','(878) 179-6603');
+INSERT INTO `customer` (`IDCUSTOMER`,`PRIMARY`,`NAME`,`REVENUE`,`PROFIT`,`STREET`,`CITY`,`STATE`,`ZIP`,`PHONE`) VALUES (2,'Robert','Exxon Mobil',5661,585,'1309 Burning Trail','Yazoo City','NE','68970-0108','(531) 984-8463');
+INSERT INTO `customer` (`IDCUSTOMER`,`PRIMARY`,`NAME`,`REVENUE`,`PROFIT`,`STREET`,`CITY`,`STATE`,`ZIP`,`PHONE`) VALUES (3,'John','U.S. Steel',3250,195,'3930 Iron Walk','Wild Horse','MO','65453-3393','(417) 591-1188');
+INSERT INTO `customer` (`IDCUSTOMER`,`PRIMARY`,`NAME`,`REVENUE`,`PROFIT`,`STREET`,`CITY`,`STATE`,`ZIP`,`PHONE`) VALUES (4,'Michael','General Electric',2959,213,'4363 Stony Crescent','Ste. Rose du Lac','CA','92186-9180','(650) 151-3276');
+INSERT INTO `customer` (`IDCUSTOMER`,`PRIMARY`,`NAME`,`REVENUE`,`PROFIT`,`STREET`,`CITY`,`STATE`,`ZIP`,`PHONE`) VALUES (5,'David','Esmark',2511,19,'9443 Colonial Berry Canyon','Fleming','DE','19753-5942','(302) 030-2953');
+INSERT INTO `customer` (`IDCUSTOMER`,`PRIMARY`,`NAME`,`REVENUE`,`PROFIT`,`STREET`,`CITY`,`STATE`,`ZIP`,`PHONE`) VALUES (6,'William','Chrysler',2072,19,'9386 Easy Vista','Wall City','MO','65266-8602','(660) 918-4630');
+INSERT INTO `customer` (`IDCUSTOMER`,`PRIMARY`,`NAME`,`REVENUE`,`PROFIT`,`STREET`,`CITY`,`STATE`,`ZIP`,`PHONE`) VALUES (7,'Richard','Armour',2056,2,'532 Umber Lake Highlands','Englefeld','RI','02956-5619','(401) 002-1176');
+INSERT INTO `customer` (`IDCUSTOMER`,`PRIMARY`,`NAME`,`REVENUE`,`PROFIT`,`STREET`,`CITY`,`STATE`,`ZIP`,`PHONE`) VALUES (8,'Thomas','Gulf Oil',1705,183,'4220 Rocky Wynd','Bolivia','MN','56253-0991','(952) 991-4731');
+INSERT INTO `customer` (`IDCUSTOMER`,`PRIMARY`,`NAME`,`REVENUE`,`PROFIT`,`STREET`,`CITY`,`STATE`,`ZIP`,`PHONE`) VALUES (9,'Charles','Mobil',1704,184,'5016 High Fawn Path','Boissevain','OK','73763-3294','(539) 389-7200');
+INSERT INTO `customer` (`IDCUSTOMER`,`PRIMARY`,`NAME`,`REVENUE`,`PROFIT`,`STREET`,`CITY`,`STATE`,`ZIP`,`PHONE`) VALUES (10,'Gary','DuPont',1688,344,'7151 Indian Limits','Mock City','NE','69497-1440','(308) 186-7323');
+INSERT INTO `customer` (`IDCUSTOMER`,`PRIMARY`,`NAME`,`REVENUE`,`PROFIT`,`STREET`,`CITY`,`STATE`,`ZIP`,`PHONE`) VALUES (11,'Larry','Amoco',1667,133,'8109 Quaking Extension','Freeze Fork','RI','02823-7971','(401) 842-8870');
+INSERT INTO `customer` (`IDCUSTOMER`,`PRIMARY`,`NAME`,`REVENUE`,`PROFIT`,`STREET`,`CITY`,`STATE`,`ZIP`,`PHONE`) VALUES (12,'Ronald','Bethlehem Steel',1660,117,'9717 Round Drive','Hardshell','OK','73108-1789','(918) 523-7811');
+INSERT INTO `customer` (`IDCUSTOMER`,`PRIMARY`,`NAME`,`REVENUE`,`PROFIT`,`STREET`,`CITY`,`STATE`,`ZIP`,`PHONE`) VALUES (13,'Joseph','CBS',1631,85,'8245 Lazy Cloud Crest','Treetops','MO','65194-7190','(636) 739-9890');
+INSERT INTO `customer` (`IDCUSTOMER`,`PRIMARY`,`NAME`,`REVENUE`,`PROFIT`,`STREET`,`CITY`,`STATE`,`ZIP`,`PHONE`) VALUES (14,'Donald','Texaco',1574,226,'2820 Amber Acres','Maple Creek','WI','54112-5496','(534) 839-0049');
+INSERT INTO `customer` (`IDCUSTOMER`,`PRIMARY`,`NAME`,`REVENUE`,`PROFIT`,`STREET`,`CITY`,`STATE`,`ZIP`,`PHONE`) VALUES (15,'Kenneth','AT&T Technologies',1526,56,'5414 Hidden Byway','Beersville','HI','96732-5653','(808) 216-1044');
+INSERT INTO `customer` (`IDCUSTOMER`,`PRIMARY`,`NAME`,`REVENUE`,`PROFIT`,`STREET`,`CITY`,`STATE`,`ZIP`,`PHONE`) VALUES (16,'Steven','Shell Oil',1312,121,'2638 Lost Robin Alley','Warroad','MO','64001-0474','(417) 976-4146');
+INSERT INTO `customer` (`IDCUSTOMER`,`PRIMARY`,`NAME`,`REVENUE`,`PROFIT`,`STREET`,`CITY`,`STATE`,`ZIP`,`PHONE`) VALUES (17,'Dennis','Kraft',1210,37,'2789 Silent Vale','Charcoal Landing','MO','64075-9452','(636) 650-5989');
+INSERT INTO `customer` (`IDCUSTOMER`,`PRIMARY`,`NAME`,`REVENUE`,`PROFIT`,`STREET`,`CITY`,`STATE`,`ZIP`,`PHONE`) VALUES (18,'Paul','ChevronTexaco',1113,212,'342 Blue Key','Zealandia','MN','56268-4891','(952) 107-5495');
+INSERT INTO `customer` (`IDCUSTOMER`,`PRIMARY`,`NAME`,`REVENUE`,`PROFIT`,`STREET`,`CITY`,`STATE`,`ZIP`,`PHONE`) VALUES (19,'Stephen','Goodyear Tire & Rubber',1090,48,'5479 Silver Villas','Effort','MN','56108-2359','(763) 274-6998');
+INSERT INTO `customer` (`IDCUSTOMER`,`PRIMARY`,`NAME`,`REVENUE`,`PROFIT`,`STREET`,`CITY`,`STATE`,`ZIP`,`PHONE`) VALUES (20,'George','Boeing',1033,37,'8680 Velvet Avenue','Sceptre','WI','54252-7174','(608) 776-1393');
