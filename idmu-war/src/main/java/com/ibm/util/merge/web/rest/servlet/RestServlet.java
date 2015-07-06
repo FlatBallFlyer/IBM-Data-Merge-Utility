8cbdd5327e5d77c4fe758c902ba0b93c02ab1073
Jonathan Bosmans <jonathan.bosmans@be.ibm.com>
2015-07-06 21:29:29 +0200

    Adding custom rest servlet - no framework

---
+++ idmu-war/src/main/java/com/ibm/util/merge/web/rest/servlet/RestServlet.java
@@ -0,0 +1,42 @@
+package com.ibm.util.merge.web.rest.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ *
+ */
+public class RestServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
+        super.doGet(httpServletRequest, httpServletResponse);
+    }
+
+    @Override
+    protected void doHead(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
+        super.doHead(httpServletRequest, httpServletResponse);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
+        super.doPost(httpServletRequest, httpServletResponse);
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
+        super.doPut(httpServletRequest, httpServletResponse);
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
+        super.doDelete(httpServletRequest, httpServletResponse);
+    }
+
+    @Override
+    protected void doOptions(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
+        super.doOptions(httpServletRequest, httpServletResponse);
+    }
+}
