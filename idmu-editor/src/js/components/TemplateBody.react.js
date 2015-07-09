c77c6e05370bd0903ca82fa171aefefb2519d996
Yesh <yesh@elorasoft.com>
2015-07-09 10:20:01 -0400

    bookmark modal and insert at caret

--- idmu-editor/src/js/components/TemplateBody.react.js
+++ idmu-editor/src/js/components/TemplateBody.react.js
@@ -2,6 +2,70 @@
  * @jsx React.DOM
  */
 
+var InsertBookmarkTrigger = React.createClass({
+  handleClick: function(e) {
+    console.log("open modal");
+    var el = this.refs.bookmark_editor.getDOMNode();
+    $(el).modal();
+  },
+  render: function() {
+    var iCB = this.props.iCB;
+    return(
+      <div>
+        <span className="btn btn-primary" onClick={this.handleClick}>Insert Template</span>
+        <BookmarkEditor  key={this.props.title}  ref="bookmark_editor" title={this.props.title} iCB={iCB}/>
+      </div>
+    );
+  }
+});
+
+var BookmarkEditor = React.createClass({
+  mixins: [TextEditMixin,ModalMixin],
+  getInitialState: function() {
+    return({collection:'', name: '', columnName: ''});
+  },
+  handleSave: function(e) {
+    this.props.iCB(this.state.collection,this.state.name,this.state.columnName);
+  },
+  render: function() {
+    return (
+      <div onClick={this.handleClick} className="modal" role="dialog" aria-hidden="true">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h3>Insert Bookmark</h3>
+            </div>
+            <div className="modal-body">
+              <div className="row">
+                <div className="col-lg-12">
+                  <form>
+                    <div className="form-group col-xs-6 col-md-6">
+                      <label for="description" className="control-label">Collection</label>
+                      <input  onChange={this.handleTextEditChange} className="form-control" id="collection" type="text" value={this.state.collection}/>
+                    </div>
+                    <div className="form-group col-xs-6 col-md-6">
+                      <label for="description" className="control-label">Name</label>
+                      <input  onChange={this.handleTextEditChange} className="form-control" id="name" type="text" value={this.state.name}/>
+                    </div>
+                    <div className="form-group col-xs-6 col-md-6">
+                      <label for="description" className="control-label">Column Name</label>
+                      <input  onChange={this.handleTextEditChange} className="form-control" id="columnName" type="text" value={this.state.columnName}/>
+                    </div>
+                  </form>
+                </div>
+              </div>              
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-default" data-dismiss="modal">Cancel</button>
+              <button onClick={this.handleSave} type="button" className="btn btn-primary">Insert</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+});
+
 var ContentEditable = React.createClass({
     render: function(){
       return(<div id="contenteditable"
@@ -18,25 +82,29 @@ var ContentEditable = React.createClass({
     componentDidUpdate: function() {
         if ( this.props.html !== this.getDOMNode().innerHTML ) {
           var els = $.parseHTML(this.props.html);
-          els.forEach(function(el){
-            if($(el).prop('tagName') == "DIV" && $(el).attr("class") === "tkbookmark"){
-              $(el).empty();
-              var escd = $(el).prop('outerHTML').replace(/[\u00A0-\u9999<>\&]/gim, function(i) {
-                return '&#' + i.charCodeAt(0) + ';';
-              });
-              var tip = "collection="+$(el).attr("collection")+",name="+$(el).attr("name");
-              if($(el).attr("column")) {
-                tip+= ",column="+$(el).attr("column");
+          if(els){
+            els.forEach(function(el){
+              if($(el).prop('tagName') == "DIV" && $(el).attr("class") === "tkbookmark"){
+                $(el).empty();
+                var escd = $(el).prop('outerHTML').replace(/[\u00A0-\u9999<>\&]/gim, function(i) {
+                  return '&#' + i.charCodeAt(0) + ';';
+                });
+                var tip = "collection="+$(el).attr("collection")+",name="+$(el).attr("name");
+                if($(el).attr("column")) {
+                  tip+= ",column="+$(el).attr("column");
+                }
+                var a=$("<input title=\""+tip+"\" class=\"toberemoved inline btn btn-warning btn-xs\" type=\"button\" value=\"tkBookmark\"/>");
+                $(el).append(a);
               }
-              var a=$("<input title=\""+tip+"\" class=\"toberemoved inline btn btn-warning btn-xs\" type=\"button\" value=\"tkBookmark\"/>");
-              $(el).append(a);
-            }
-          });
-          var final_el = $("<div/>");
-          els.forEach(function(el){
-            $(final_el).append(el);
-          });
-          this.getDOMNode().innerHTML = final_el.prop('innerHTML');//this.props.html;
+            });
+            var final_el = $("<div/>");
+            els.forEach(function(el){
+              $(final_el).append(el);
+            });
+            this.getDOMNode().innerHTML = final_el.prop('innerHTML');//this.props.html;
+          }else {
+            this.getDOMNode().innerHTML = this.props.html;
+          }
         }
     },
     emitChange: function(){
@@ -89,15 +157,19 @@ var TemplateBody = React.createClass({
       }
     }
   },
-  handleInsert: function(){
+  handleInsert: function(colllection,name,columnName){
     var el = document.getElementById('contenteditable');
     el.focus();
-    var tip = "collection=blah,blah blah";
-    var bkMark="<div class=\"tkbookmark\" collection=\"mame\" name=\"myname\"><input title=\""+tip+"\" class=\"toberemoved inline btn btn-warning btn-xs\" type=\"button\" value=\"tkBookmark..\"/></div>";
+    var tip = "collection="+collection+"name="+name;
+    if(columnName && columnName.length > 0){
+      tip+=",column="+columnName;
+    }
+    var bkMark="<div class=\"tkbookmark\" collection=\""+collection+"\" name=\""+name+"\" columnName=\""+columnName+"\"><input title=\""+tip+"\" class=\"toberemoved inline btn btn-warning btn-xs\" type=\"button\" value=\"tkBookmark..\"/></div>";
     this.pasteHtmlAtCaret(bkMark);
     this.setState({content: $(el).prop('innerHTML')});
   },
   render: function(){
+    var title = "Insert Bookmark";
     return(
       <div className="row no-margin      ribbon-inside">
         <form>
@@ -106,11 +178,14 @@ var TemplateBody = React.createClass({
               <label for="name" className="control-label">Content</label>
                 <ContentEditable html={this.state.content} id="content" onChange={this.handleContentChange}/>
             </div>
-            <div className="form-group col-xs-12 col-md-12">
-              <input onClick={this.handleInsert} type="button" className="btn btn-primary" value="Insert Template"/>
-            </div>            
           </div>
         </form>
-      </div>);
+        <div>
+          <div className="form-group col-xs-12 col-md-12">
+            <InsertBookmarkTrigger title={title} iCB={this.handleInsert}/>
+          </div>            
+        </div>
+      </div>
+    );
   }
 });
