38f3da5d5cc675178b791861c729043533eb63d5
Mike Storey <mike@ssgpp.com>
2015-07-25 13:07:43 -0400

    Copyright and License in all Java source code

--- idmu-util/src/main/java/com/ibm/util/merge/db/ConnectionPoolManager.java
+++ idmu-util/src/main/java/com/ibm/util/merge/db/ConnectionPoolManager.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2015, 2015 IBM
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 package com.ibm.util.merge.db;
 
 import com.ibm.idmu.api.DatabaseConnectionProvider;
@@ -127,6 +143,7 @@ public class ConnectionPoolManager implements PoolManager {
         return connectionProviders.get(poolName).statistics();
     }
 
+    @Override
     public Connection acquireConnection(String poolName){
         return connectionProviders.get(poolName).acquireConnection();
     }
