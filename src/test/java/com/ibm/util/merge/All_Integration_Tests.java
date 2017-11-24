2cbad26eb1ce2f3d61329c752a5b026ab12648f9
Mike Storey <mike@ssgpp.com>
2017-11-24 12:41:50 -0500

    MySql Testing Update
    Moved the PW into Dockerfile

--- src/test/java/com/ibm/util/merge/All_Integration_Tests.java
+++ src/test/java/com/ibm/util/merge/All_Integration_Tests.java
@@ -8,7 +8,7 @@ public class All_Integration_Tests {
 	// In order to run these integration tests you need to run the docker testing container
 	// which runs all the required test databases. If you have not already done so
 	// install Docker - https://www.docker.com and then run the following command
-	// docker run -p 3306:3306 --name idmu-mysql -e MYSQL_ROOT_PASSWORD=drowssap -d flatballflyer/idmu-test-mysql:1
+	// docker run -p 3306:3306 --name idmu-mysql -d flatballflyer/idmu-test-mysql:latest
 	// 
 	// NOTE: If you change the root password you will need to change the corresponding Config Environment variables for the JDBC data source.
 	// TODO: This is just a MySql container, will implement as a full stack with PWD's hidden
