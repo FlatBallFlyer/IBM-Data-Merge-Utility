806fafcf726760a47cf9c6b75b51e72aa0631053
Mike Storey <mike@ssgpp.com>
2017-11-28 17:14:55 -0500

    Added all tests

---
+++ src/test/java/com/ibm/util/merge/ALL_TESTS.java
@@ -0,0 +1,16 @@
+package com.ibm.util.merge;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ 
+	com.ibm.util.merge.All_Unit_Tests.class,
+	com.ibm.util.merge.All_Functional_Tests.class,
+	com.ibm.util.merge.All_Integration_Tests.class
+})
+
+public class All_TESTS {
+
+}
